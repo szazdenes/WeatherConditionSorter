77815dbb862a4d638c05241845881163f04c0e6b
szazdenes <szaz.denes@gmail.com>
2017-12-05 19:35:19 +0100

    wind and temperature conditions counted

--- sorterform.cpp
+++ sorterform.cpp
@@ -6,9 +6,92 @@ SorterForm::SorterForm(QWidget *parent) :
     ui(new Ui::SorterForm)
 {
     ui->setupUi(this);
+    ui->windMinDoubleSpinBox->setValue(1);
+    ui->windMaxDoubleSpinBox->setValue(10);
+    ui->windRangeDoubleSpinBox->setValue(1);
+    ui->tempMinDoubleSpinBox->setValue(25);
+    ui->tempMaxDoubleSpinBox->setValue(37);
+    ui->tempRangeDoubleSpinBox->setValue(1);
 }
 
 SorterForm::~SorterForm()
 {
     delete ui;
+}
+
+void SorterForm::on_pushButton_clicked()
+{
+    fileName = QFileDialog::getOpenFileName(this, "Open file", "../../", "*.dat");
+    loadData(fileName);
+    selectData(weatherDataMap);
+}
+
+void SorterForm::loadData(QString fileName)
+{
+    if (!fileName.isEmpty()){
+        QFile file(fileName);
+        if(!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            qDebug("File cannot be opened.");
+            return;
+        }
+
+        QPair<double, double> dataPair;
+        QTextStream stream(&file);
+        QString date;
+        double temp, wind;
+        stream.readLine();
+        while(!stream.atEnd()){
+            QString line = stream.readLine();
+            QTextStream lineStream(&line);
+            lineStream >> date >> temp >> wind;
+            dataPair.first = temp;
+            dataPair.second = wind;
+            weatherDataMap[date].append(dataPair);
+        }
+        file.close();
+    }
+}
+
+void SorterForm::selectData(QMap<QString, QList<QPair<double, double> > > dataMap)
+{
+    if(!dataMap.isEmpty()){
+        for(double i = ui->tempMinDoubleSpinBox->value(); i <= ui->tempMaxDoubleSpinBox->value(); i += ui->tempRangeDoubleSpinBox->value()){
+
+            QString month = fileName.split("/").last().remove(".dat");
+            QFile outFile("../results/" + month + "_T_" + QString::number(i) + "_.csv");
+            if(!outFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+                qDebug("File cannot be opened.");
+                return;
+            }
+            QTextStream outStream(&outFile);
+
+            for(double j = ui->windMinDoubleSpinBox->value(); j <= ui->windMaxDoubleSpinBox->value(); j += ui->windRangeDoubleSpinBox->value()){
+                QMap<QString, int> resultMap;
+                foreach(QString currentKey, dataMap.keys()){
+                    for(int k = 0; k < dataMap[currentKey].size(); k++){
+                        if(dataMap[currentKey].at(k).first > i && dataMap[currentKey].at(k).second < j){
+                            resultMap[currentKey]++;
+                        }
+                    }
+                }
+
+                outStream << "#time/wind\n";
+                outStream << QString::number(j) + "\t";
+                QStringList keyList = resultMap.keys();
+                qSort(keyList.begin(), keyList.end());
+                foreach(QString currKey, keyList){
+                    if(currKey != keyList.last())
+                        outStream << QString::number(resultMap[currKey]) + "\t";
+                    else
+                        outStream << QString::number(resultMap[currKey]) + "\n";
+                }
+            }
+            outFile.close();
+        }
+    }
+}
+
+void SorterForm::on_pushButton_3_clicked()
+{
+
 }
--- sorterform.h
+++ sorterform.h
@@ -2,6 +2,9 @@
 #define SORTERFORM_H
 
 #include <QWidget>
+#include <QMap>
+#include <QFileDialog>
+#include <QTextStream>
 
 namespace Ui {
 class SorterForm;
@@ -15,8 +18,18 @@ public:
     explicit SorterForm(QWidget *parent = 0);
     ~SorterForm();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_3_clicked();
+
 private:
     Ui::SorterForm *ui;
+    QMap<QString, QList<QPair<double, double> > > weatherDataMap;
+    QString fileName;
+
+    void loadData(QString fileName);
+    void selectData(QMap<QString, QList<QPair<double, double> > > dataMap); //first: air temp, second: wind
 };
 
 #endif // SORTERFORM_H
